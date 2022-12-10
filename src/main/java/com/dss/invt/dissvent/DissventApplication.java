2e40eb7d302587711d9954fd78ab72f73f2ffa9d
danielss24 <daniels_s_24@hotmail.com>
2022-12-10 20:04:10 +0100

    Basic Spring Init

---
+++ src/main/java/com/dss/invt/dissvent/DissventApplication.java
@@ -0,0 +1,13 @@
+package com.dss.invt.dissvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dss/invt/dissvent/DissventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.invt.dissvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
